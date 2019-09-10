94e3b7de4bc1cfe3a905ae1fdc80f097dd83a8f8
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-10 16:06:02 +0100

    use map to iterate over the array data

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -13,6 +13,9 @@ const Specials = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {
+         specialCharacters.map(special => <SpecialButton specialCharacter={special} onTap = {(special) => onTap(special)} />)
+       }
     </div>
   );
 };

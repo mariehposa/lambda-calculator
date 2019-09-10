2529198992a6ce4e11b10e3fba1d13fd13c9079e
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-10 15:28:38 +0100

    use map to iterate over the array data

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -18,6 +18,10 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+
+      {
+        stateNumbers.map(number => <NumberButton number={number} onTap={(whatComesIn) => onTap(whatComesIn)}/>)
+      }
     </div>
   );
 };
